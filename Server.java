00c5feca59b0cc664fbf629a8a0a0197e2577b5e
IrouKaizen <122926735+IrouKaizen@users.noreply.github.com>
2023-11-29 14:54:24 +0000

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,9 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package tp0;
+package Point;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,33 +12,104 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author ODG
+ * @author _irk
  */
-public class Server {
+public class Server implements Runnable{
     
-    public static void main(String[] iren) throws IOException {
-        ServerSocket ss = new ServerSocket(1234);
-        Socket s = ss.accept();
+    List<Chat> listClients = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+         new Thread(new Server()).start(); 
+    }
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket ss = new ServerSocket(1234);
+            System.out.println("Démarrage du serveur");
+            
+            Socket s = ss.accept();
+            //Lecture de données
+                 
+
+                 String pseudo = null;
+                 Chat c = new Chat(s,pseudo);
+                    c.start();
+                    listClients.add(c);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private class Chat extends Thread{
+         private Socket socket;
+         private String pseudo;
+        public Chat(Socket s, String pseudo) {
+            this.socket=s;
+            this.pseudo = pseudo;
+        }
+
+        public Socket getSocket() {
+            return socket;
+        }
+
+        public void setSocket(Socket socket) {
+            this.socket = socket;
+        }
+
+        public String getPseudo() {
+            return pseudo;
+        }
+
+        public void setPseudo(String pseudo) {
+            this.pseudo = pseudo;
+        }
         
-        //lecture de données
-        InputStream is = s.getInputStream();
-        InputStreamReader isr = new InputStreamReader(is);
-        BufferedReader br = new BufferedReader(isr);
         
-        //Ecriture de données
-        OutputStream os = s.getOutputStream();
-        PrintWriter pw = new PrintWriter(os, true);
-        
-        String IP = s.getRemoteSocketAddress().toString();
-        pw.println("Bienvenue client IP= " +IP);
-        String chaine = br.readLine();
-        int n = chaine.length();
-        
-        pw.println("la longueur de la chaine est :" +n);
+
+        @Override
+        public void run() {
+            
+             try {
+                 //Lecture de données
+                 
+                 InputStream is = socket.getInputStream();
+                 InputStreamReader isr = new InputStreamReader(is);
+                 BufferedReader br = new BufferedReader(isr);
+                 
+                 //Ecriture de données
+                 
+                 OutputStream os = socket.getOutputStream();
+                 PrintWriter pw = new PrintWriter(os,true);
+                 String IP = socket.getRemoteSocketAddress().toString();
+                 
+                 while (true) {
+                    pw.println("Veuillez entrer un pseudo: ");
+                    pseudo = br.readLine();
+                    
+                    boolean isExiste = false;
+                   
+                    while ("".equals(pseudo)) {
+                       pw.println("Le Pseudo ne peut pas etre nul: ");
+                       pseudo = br.readLine();
+                    } 
+                    pw.println("Bienvenue client " +pseudo);
+                 }
+                 
+                 
+             } catch (IOException ex) {
+                 Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+             } 
+
+        }
         
     }
-  
+    
 }
